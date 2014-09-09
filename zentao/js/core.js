511c85cfb86330ad55a1a871a34b422684b1f5d9
Catouse <catouse@gmail.com>
2014-09-09 16:14:57 +0800

    + added core functions.

---
+++ zentao/js/core.js
@@ -0,0 +1,219 @@
+if (typeof Array.isArray === 'undefined')
+{
+    Array.isArray = function(obj)
+    {
+        return Object.toString.call(obj) === '[object Array]';
+    }
+};
+
+if (!Array.prototype.forEach)
+{
+    Array.prototype.forEach = function(fun /*, thisp*/)
+    {
+        var len = this.length;
+        if (typeof fun != "function")
+            throw new TypeError();
+
+        var thisp = arguments[1];
+        for (var i = 0; i < len; i++)
+        {
+            if (i in this)
+                fun.call(thisp, this[i], i, this);
+        }
+  };
+};
+
+Date.ONEDAY_TICKS = 24 * 3600 * 1000;
+
+/**
+ * Format date to a string
+ *
+ * @param  string   format
+ * @return string
+ */
+Date.prototype.format = function(format)
+{
+    var date =
+    {
+        "M+": this.getMonth() + 1,
+        "d+": this.getDate(),
+        "h+": this.getHours(),
+        "m+": this.getMinutes(),
+        "s+": this.getSeconds(),
+        "q+": Math.floor((this.getMonth() + 3) / 3),
+        "S+": this.getMilliseconds()
+    };
+    if (/(y+)/i.test(format))
+    {
+        format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+    }
+    for (var k in date)
+    {
+        if (new RegExp("(" + k + ")").test(format))
+        {
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? date[k] : ("00" + date[k]).substr(("" + date[k]).length));
+        }
+    }
+    return format;
+};
+
+Date.prototype.addMilliseconds = function(value)
+{
+    this.setTime(this.getTime() + value);
+    return this;
+};
+
+Date.prototype.addDays = function(days)
+{
+    this.addMilliseconds(days * Date.ONEDAY_TICKS);
+    return this;
+};
+
+Date.prototype.clone = function()
+{
+    var date =new Date();
+    date.setTime(this.getTime());
+    return date;
+};
+
+Date.isLeapYear = function (year)
+{
+    return (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0));
+};
+
+Date.getDaysInMonth = function (year, month)
+{
+    return [31, (Date.isLeapYear(year) ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
+};
+
+Date.prototype.isLeapYear = function ()
+{
+    var y = this.getFullYear();
+    return (((y % 4 === 0) && (y % 100 !== 0)) || (y % 400 === 0));
+};
+
+Date.parseName = function(name)
+{
+    var date = new Date();
+    switch(name)
+    {
+        case 'now':
+            break;
+        case 'today':
+            date.clearTime();
+            break;
+        case 'yestoday':
+            date.clearTime().addDays(-1);
+            break;
+        case 'thisweek':
+            date = date.getLastWeekday();
+            break;
+        case 'lastweek':
+            date = date.getLastWeekday().addDays(-7);
+            break;
+        case 'thisyear':
+            date.clearTime();
+            date.setMonth(1);
+            date.setDate(1);
+            break;
+    }
+    return date;
+};
+
+Date.prototype.clearTime = function()
+{
+    this.setHours(0);
+    this.setMinutes(0);
+    this.setSeconds(0);
+    this.setMilliseconds(0);
+    return;
+};
+
+Date.prototype.getDaysInMonth = function ()
+{
+    return Date.getDaysInMonth(this.getFullYear(), this.getMonth());
+};
+
+Date.prototype.addMonths = function (value)
+{
+    var n = this.getDate();
+    this.setDate(1);
+    this.setMonth(this.getMonth() + value);
+    this.setDate(Math.min(n, this.getDaysInMonth()));
+    return this;
+};
+
+Date.prototype.getLastWeekday = function(day)
+{
+    day = day || 1;
+
+    var d = date.clone();
+    while(d.getDay() != day)
+    {
+        d.addDays(-1);
+    }
+    d.clearTime();
+    return d;
+};
+
+// Date.prototype.isSameDay = function(date)
+// {
+//     return date.toDateString() === this.toDateString();
+// };
+
+/**
+ * Format string
+ *
+ * @param  object|array args
+ * @return string
+ */
+String.prototype.format = function(args)
+{
+    var result = this;
+    if (arguments.length > 0)
+    {
+        var reg;
+        if (arguments.length == 1 && typeof(args) == "object")
+        {
+            for (var key in args)
+            {
+                if (args[key] != undefined)
+                {
+                    reg = new RegExp("({" + key + "})", "g");
+                    result = result.replace(reg, args[key]);
+                }
+            }
+        }
+        else
+        {
+            for (var i = 0; i < arguments.length; i++)
+            {
+                if (arguments[i] != undefined)
+                {
+                    reg = new RegExp("({[" + i + "]})", "g");
+                    result = result.replace(reg, arguments[i]);
+                }
+            }
+        }
+    }
+    return result;
+};
+
+
+/**
+ * Judge the string is a integer number
+ *
+ * @access public
+ * @return bool
+ */
+String.prototype.isNum = function(s)
+{
+    if(s!=null)
+    {
+        var r, re;
+        re = /\d*/i;
+        r = s.match(re);
+        return (r == s) ? true : false;
+    }
+    return false;
+}
