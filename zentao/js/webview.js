bbee798eb08dee9118c59f3e0720aaf54b2f4bbf
Catouse <catouse@gmail.com>
2014-09-10 10:33:27 +0800

    * ajusted code format.

--- zentao/js/webview.js
+++ zentao/js/webview.js
@@ -1,8 +1,9 @@
 /* 全局共享变量 */
-(function(mui, $) {
-
+(function(mui, $)
+{
     // 空函数
-    function shield() {
+    function shield()
+    {
         return false;
     }
 
@@ -10,12 +11,14 @@
     // document.oncontextmenu=shield;//屏蔽选择函数
 
     //全局配置(通常所有页面引用该配置，特殊页面使用mui.init({})来覆盖全局配置)
-    mui.initGlobal({
+    mui.initGlobal(
+    {
         optimize: true,
         swipeBack: true,
         showAfterLoad: true,
         titleBar: false,
-        show: {
+        show:
+        {
             aniShow: 'slide-in-right',
             duration: 400
         }
@@ -23,31 +26,40 @@
 
     mui.init();
 
-    mui.plusReady(function() {
+    mui.plusReady(function()
+    {
         var url = plus.webview.currentWebview().getURL();
         console.color('WEBVIEW 准备好了！ [' + url.substring(url.lastIndexOf('/')) + ']', 'h1|bgmuted');
 
         // store.setPlusStorage(plus);
 
         // bind events
-        $('body').on('tap', 'a', function(e) {
+        $('body').on('tap', 'a', function(e)
+        {
             var id = this.getAttribute('href');
-            if (id && ~id.indexOf('.html')) {
-                if (window.plus) {
-                    $.openWindow({
+            if (id && ~id.indexOf('.html'))
+            {
+                if (window.plus)
+                {
+                    $.openWindow(
+                    {
                         id: id,
                         url: this.href,
                         preload: true //TODO 等show，hide事件，动画都完成后放开预加载
                     });
-                } else {
+                }
+                else
+                {
                     document.location.href = this.href;
                 }
             }
         });
 
         // Android处理返回键
-        plus.key.addEventListener('backbutton', function() {
-            if (confirm('确认退出？')) {
+        plus.key.addEventListener('backbutton', function()
+        {
+            if (confirm('确认退出？'))
+            {
                 plus.runtime.quit();
             }
         }, false);
@@ -56,22 +68,33 @@
     /**
      * hyperlink
      */
-    mui.ready(function() {
-        mui('body').on('tap', 'a', function(e) {
+    mui.ready(function()
+    {
+        mui('body').on('tap', 'a', function(e)
+        {
             var id = this.getAttribute('href');
-            if (id) {
-                if (~id.indexOf('.html')) {
-                    if (window.plus) {
-                        mui.openWindow({
+            if (id)
+            {
+                if (~id.indexOf('.html'))
+                {
+                    if (window.plus)
+                    {
+                        mui.openWindow(
+                        {
                             id: id,
                             url: this.href,
                             preload: $.os.ios ? false : true //TODO 暂时屏蔽IOS的预加载
                         });
-                    } else {
+                    }
+                    else
+                    {
                         document.location.href = this.href;
                     }
-                } else {
-                    if (typeof plus !== 'undefined') {
+                }
+                else
+                {
+                    if (typeof plus !== 'undefined')
+                    {
                         plus.runtime.openURL(id);
                     }
                 }
