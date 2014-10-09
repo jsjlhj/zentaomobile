48139d5049780748e5377ab36671b597ecc5e401
Catouse <catouse@gmail.com>
2014-10-09 19:24:57 +0800

    + added store.js to zui.

---
+++ zui/src/js/core/store.js
@@ -0,0 +1,138 @@
+define(['json.date'], function(JSON)
+{
+    JSON.useDateParser();
+
+    var lsName = 'localStorage';
+    var storage = window[lsName],
+        old = window['store'];
+
+    /* The Store object */
+    var Store = function() {
+        this.slience = true;
+        this.enable = (lsName in window) && window[lsName] && window[lsName]['setItem'];
+        this.storage = storage;
+    };
+
+    /* Set storage */
+    Store.prototype.setStorage = function(st) {
+        if (st && st.getLength) {
+            this.enable = true;
+            storage = st;
+            this.storage = st;
+        }
+        return this;
+    };
+
+    /* Check enable status */
+    Store.prototype.check = function() {
+        if (!this.enable) {
+            if (!this.slience) throw new Error('Browser not support localStorage or enable status been set true.');
+        }
+        return this.enable;
+    };
+
+    /* Get length */
+    Store.prototype.length = function() {
+        if (this.check()) {
+            return storage.getLength ? storage.getLength() : storage.length;
+        }
+        return 0;
+    };
+
+    /* Remove item with browser localstorage native method */
+    Store.prototype.removeItem = function(key) {
+        storage.removeItem(key);
+        return this;
+    };
+
+    /* Remove item with browser localstorage native method, same as removeItem */
+    Store.prototype.remove = function(key) {
+        return this.removeItem(key);
+    };
+
+    /* Get item value with browser localstorage native method, and without deserialize */
+    Store.prototype.getItem = function(key) {
+        return storage.getItem(key);
+    };
+
+    /* Get item value and deserialize it, if value is null and defaultValue been given then return defaultValue */
+    Store.prototype.get = function(key, defaultValue) {
+        var val = this.deserialize(this.getItem(key));
+        return (defaultValue !== undefined && (typeof val === 'undefined' || val === null || val === undefined)) ? defaultValue : val;
+    };
+
+    /* Get item key by index and deserialize it */
+    Store.prototype.key = function(index) {
+        return storage.key(key);
+    };
+
+    /* Set item value with browser localstorage native method, and without serialize filter */
+    Store.prototype.setItem = function(key, val) {
+        storage.setItem(key, val);
+        return this;
+    };
+
+    /* Set item value, serialize it if the given value is not an string */
+    Store.prototype.set = function(key, val) {
+        if (val === undefined) return this.remove(key);
+        this.setItem(key, this.serialize(val));
+        console.groupCollapsed('%cSTORE SET: ' + key, 'color: purple; border-left: 10px solid orange; padding-left: 5px;font-size: 14px; font-weight: bold;');
+        console.log(val);
+        console.log('JSON:', this.serialize(val));
+        console.groupEnd();
+        return this;
+    };
+
+    /* Clear all items with browser localstorage native method */
+    Store.prototype.clear = function() {
+        storage.clear();
+        return this;
+    };
+
+    /* Iterate all items with callback */
+    Store.prototype.forEach = function(callback) {
+        for (var i = storage.length - 1; i >= 0; i--) {
+            var key = storage.key(i);
+            callback(key, store.get(key));
+        }
+        return this;
+    };
+
+    /* Get all items and set value in an object. */
+    Store.prototype.getAll = function(callback) {
+        var all = {};
+        this.forEach(function(key, val) {
+            all[key] = val;
+        });
+
+        return all;
+    };
+
+    /* Serialize value with JSON.stringify */
+    Store.prototype.serialize = function(value) {
+        if (typeof value === 'string') return value;
+        return JSON.stringify(value);
+    };
+
+    /* Deserialize value, with JSON.parse if the given value is not a string */
+    Store.prototype.deserialize = function(value) {
+        if (typeof value !== 'string') return undefined;
+        try {
+            return JSON.parse(value);
+        } catch (e) {
+            return value || undefined;
+        }
+    };
+
+    if(!window.store)
+    {
+        window.store = new Store();
+    }
+
+    window.store.noConflict = function() {
+        window.store = old;
+        return store;
+    };
+
+    return window.store;
+});
