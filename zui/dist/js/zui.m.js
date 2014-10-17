6af42b35fe8f518447fffe84f0bf3d9a9cea6cbe
Catouse <catouse@gmail.com>
2014-10-17 20:17:14 +0800

    * added openWebview funtion to plus.js.

--- zui/dist/js/zui.m.js
+++ zui/dist/js/zui.m.js
@@ -2280,7 +2280,6 @@
 
     if(!window.fire)
     {
-
         /**
          * Trigger event on the given webview
          * @param  {object} webview 
@@ -2331,6 +2330,72 @@
              */
             window.currentWebview = window.plus.webview.currentWebview();
         });
+    }
+
+    if(!window.openWebview)
+    {
+        window.openWebview = function(webview, options)
+        {
+            if(typeof webview.show !== 'function')
+            {
+                var isStr = (typeof webview === 'string'), wv;
+                if(isStr)
+                {
+                    wv = plus.webview.getWebviewById(webview);
+                }
+                
+                if(wv)
+                {
+                    webview = wv;
+                }
+                else
+                {
+                    var viewOptions = {};
+                    if(isStr)
+                    {
+                        viewOptions.url = webview;
+                        viewOptions.id = webview;
+                    }
+                    else
+                    {
+                        viewOptions = webview;
+                    }
+                    
+                    webview = plus.webview.create(viewOptions.url, viewOptions.id || viewOptions.url, viewOptions.styles, viewOptions.extras);
+
+                    if(options === undefined)
+                    {
+                        options = {};
+                        options.waiting = viewOptions.waiting;
+                        options.aniType = viewOptions.aniType;
+                        options.duration = viewOptions.duration;
+                    }
+                }
+            }
+
+            options = Object.extend({waiting: true, aniType: 'auto', duration: 200}, options);
+
+            if(options.waiting)
+            {
+                if(options.waiting === true)
+                {
+                    options.waiting = {title: ''};
+                }
+                else if(typeof options.waiting === 'string')
+                {
+                    options.waiting = {title: options.waiting};
+                }
+                webview.waitingWindow = plus.nativeUI.showWaiting(options.title, options.options);
+            }
+
+            webview.addEventListener('loaded', function()
+            {
+                if(options.waiting) webview.waitingWindow.close();
+                webview.show(options.aniType, options.duration, options.callback);
+            });
+
+            return webview;
+        };
     }
 }());
 
