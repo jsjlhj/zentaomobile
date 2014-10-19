c50912e2fefa6e51d38dd34488857a2a7ccd380f
Catouse <catouse@gmail.com>
2014-10-19 10:15:14 +0800

    * ajusted style of views.

--- zui/dist/js/zui.m.min.js
+++ zui/dist/js/zui.m.min.js
@@ -1,5 +1,5 @@
 /*!
- * ZUI for mobile - v0.1.0-beta - 2014-10-18
+ * ZUI for mobile - v0.1.0-beta - 2014-10-19
  * http://zui.sexy
  * GitHub: https://github.com/easysoft/zui.git 
  * Copyright (c) 2014 cnezsoft.com; Licensed MIT
